1f288f9666949adae8790502789fd4efc50d67ff
Huong Nguyen <65859562+thihuong1010@users.noreply.github.com>
2023-08-09 23:32:00 +0300

    Update Program.js
    Main method is Injector Class

--- DependencyInjectionExample/Program.cs
+++ DependencyInjectionExample/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using DependencyInjectionExample;
 
+namespace DependencyInjectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an Instance of EmployeeBL and Inject the Dependency Object as an Argument to the Constructor
+            EmployeeBL employeeBL = new EmployeeBL(new EmployeeDAL());
+            List<Employee> ListEmployee = employeeBL.GetAllEmployees();
+            foreach (Employee emp in ListEmployee)
+            {
+                Console.WriteLine($"ID = {emp.ID}, Name = {emp.Name}, Department = {emp.Department}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
